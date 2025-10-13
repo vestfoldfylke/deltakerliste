2da6df3e0752c32c9e3226f5e104b7192446df8d
Håvard <havardd@365.had.no>
2025-10-13 11:20:01 +0200

    Add configuration file to export Azure AD values from environment variables

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+// Eksporter Azure AD-verdier fra miljøvariabler
+export const clientId = import.meta.env.clientId;
+export const authority = import.meta.env.authority;
+export const redirectUri = import.meta.env.redirectUri;
